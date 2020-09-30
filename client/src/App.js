78e967d2974aea4e321907a85a82a2fa2afb5006
unknown <verwiel.code@gmail.com>
2020-09-30 10:35:08 -0600

    updated favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from './assets/logo.svg'
+
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Drew -- Portfolio Website</h1>
+        <img src={Logo} alt='Logo' style={{width: '100px'}} />
       </header>
     </div>
   );
