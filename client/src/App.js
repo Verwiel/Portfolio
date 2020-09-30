b2b89a57ed5a350d27d0e8076b90d3a4323d919c
unknown <verwiel.code@gmail.com>
2020-09-30 11:06:00 -0600

    added routes and placeholder pages/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import Logo from './assets/logo.svg'
+import { Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Drew -- Portfolio Website</h1>
-        <img src={Logo} alt='Logo' style={{width: '100px'}} />
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
