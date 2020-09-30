38da9ad8c9651c1c781ce4882aaea44f7e88b7a7
unknown <verwiel.code@gmail.com>
2020-09-30 08:14:53 -0600

    Downloaded on new machine, set up styles, installed sass and react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Drew -- Portfolio Website</h1>
-        <p>testing email sync to git</p>
       </header>
     </div>
   );
