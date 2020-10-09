1a85ae15665da1f73f655e0840307e5c31041aab
Drew Verwiel <Verwiel.code@gmail.com>
2020-10-09 12:59:23 -0600

    changed the scrolled logic to just have a transparent header on the home page and visible everywhere else

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,21 +4,15 @@ import Navbar from './Navbar'
 import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(false)
   const location = useLocation()
-
-  const changeHeaderBackground = () => {
-    window.scrollY >= 20 ? setScrolled(true) : setScrolled(false)
-  }
-
-  window.addEventListener('scroll', changeHeaderBackground)
+  const activeNav = location.pathname.length > 1
 
   return (
-    <header className={scrolled ? 'global-header scrolled-header' : 'global-header'}>
+    <header className={activeNav ? 'global-header active-header' : 'global-header'}>
       <div className='global-header-content'>
         <Link to='/' className='global-header-content-logo'>
           <img src={Logo} alt='Drew Verwiel Logo' />
-          {location.pathname.length > 1 && <h1>Drew Verwiel</h1>}
+          {activeNav && <h1>Drew Verwiel</h1>}
         </Link>
 
         <Navbar />
