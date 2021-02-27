6342fb7b55054d37ea54c7d3ed8a585719de5166
unknown <verwiel.code@gmail.com>
2021-02-27 16:16:14 -0700

    adding background test image

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,8 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home'>
-        <h1>Drew Verwiel</h1>
+      <div className='home-content'>
+        <span className='home-content-name'>
+          <h2 className='home-content-name-first'>Drew</h2>
+          <h2 className='home-content-name-last'>Verwiel</h2>
+        </span>
         <p className='home-content-title'>Full Stack Developer</p>
+      </div>
     </div>
   )
 }
