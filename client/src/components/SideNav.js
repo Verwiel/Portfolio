3d223ea044bebec346115e421e8f19caf355ba73
unknown <verwiel.code@gmail.com>
2021-02-28 19:59:33 -0700

    added side nav bar for same page links and disabled scrolling on home

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -2,25 +2,27 @@ import React from 'react'
 import { NavHashLink } from 'react-router-hash-link'
 
 const SideNav = () => {
+
+  const samePageNavigation = [
+    "#hero",
+    "#about",
+    "#projects",
+    "#contact"
+  ]
+  
+  const navMap = samePageNavigation.map((nav, i) => {
+    return (
+      <li key={i}>
+        <NavHashLink to={nav} smooth className='side-link' activeClassName="side-link-active">&nbsp;
+        </NavHashLink>
+      </li>
+    )
+  })
+
   return (
     <nav className='side-bar'>
       <ul>
-        <li>
-          <NavHashLink to="/" className='side-link' activeClassName="side-link-active">&nbsp;
-          </NavHashLink>
-        </li>
-        <li>
-          <NavHashLink to="/#about" className='side-link' activeClassName="side-link-active">&nbsp; 
-          </NavHashLink>
-        </li>
-        <li>
-          <NavHashLink to="/#projects" className='side-link' activeClassName="side-link-active">&nbsp;
-          </NavHashLink>
-        </li>
-        <li>
-          <NavHashLink to="/#contact" className='side-link' activeClassName="side-link-active">&nbsp;
-          </NavHashLink>
-        </li>
+        {navMap}
       </ul>
     </nav>
   )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,69 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import useLockBodyScroll from '../hooks/useLockBodyScroll'
+import { useLocation, useHistory } from 'react-router-dom'
 import SideNav from '../components/SideNav'
 import AboutMeSection from '../components/AboutMeSection'
 import ProjectsSection from '../components/ProjectsSection'
 import ContactSection from '../components/ContactSection'
 
-const Home = () => {
+const Home = () => { 
+  const location = useLocation()
+  const history = useHistory()
+  const homeRef = useRef()
+  const aboutRef = useRef()
+  const projectsRef = useRef()
+  const contactRef = useRef()
+  useLockBodyScroll()
+
+  function scrollNavigation(e){
+    if (e.deltaY < 0){
+      // If scrolled up
+      switch(location.hash){
+        case '#about':
+          history.push('/#hero')
+          window.scrollTo({ behavior: 'smooth', top: homeRef.current.offsetTop })
+          break;
+        case '#projects':
+          history.push('/#about')
+          window.scrollTo({ behavior: 'smooth', top: aboutRef.current.offsetTop })
+          break;
+        case '#contact':
+          history.push('/#projects')
+          window.scrollTo({ behavior: 'smooth', top: projectsRef.current.offsetTop })
+          break;
+        default:
+          break;
+      }
+    } else if (e.deltaY > 0){
+      // if scrolled down
+      switch(location.hash){
+        case '':
+          history.push('/#about')
+          window.scrollTo({ behavior: 'smooth', top: aboutRef.current.offsetTop })
+          break;
+        case '#hero':
+          history.push('/#about')
+          window.scrollTo({ behavior: 'smooth', top: aboutRef.current.offsetTop })
+          break;
+        case '#about':
+          history.push('/#projects')
+          window.scrollTo({ behavior: 'smooth', top: projectsRef.current.offsetTop })
+          break;
+        case '#projects':
+          history.push('/#contact')
+          window.scrollTo({ behavior: 'smooth', top: contactRef.current.offsetTop })
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+
   return (
-    <main className='home'>
+    <main className='home' onWheel={(e) => scrollNavigation(e)}>
       <SideNav />
-      <header className='home-hero full-page'>
+      <header id='hero' className='home-hero full-page' ref={homeRef}>
         <div className='home-hero-content'>
           <p>Hey, my name is</p>
           <h1>D<span>rew </span>V<span>erwiel</span></h1>
@@ -16,9 +71,15 @@ const Home = () => {
         </div>
       </header>
 
-      <AboutMeSection />
-      <ProjectsSection />
-      <ContactSection />
+      <div ref={aboutRef}>
+        <AboutMeSection  />
+      </div>
+      <div ref={projectsRef}>
+        <ProjectsSection  />
+      </div>
+      <div ref={contactRef}>
+        <ContactSection  />
+      </div>
     </main>
   )
 }
