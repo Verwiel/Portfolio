4d0324e65b898ce9b0896268516594c01844b6d6
unknown <verwiel.code@gmail.com>
2021-03-11 19:08:27 -0700

    removed projects home styling, caused bug and needed to be redesigned anyways

--- client/src/components/StyledBackground.js
+++ client/src/components/StyledBackground.js
@@ -1,12 +1,10 @@
 import React from 'react'
-// import Ocean from '../assets/Ocean.js'
 import Moon from '../assets/Moon'
 import Boat from '../assets/Boat'
 
 const StyledBackground = () => {
   return (
     <div className='styled-background'>
-      {/* <Ocean className='styled-background' /> */}
       <Moon className='styled-background-moon' />
       <Boat className='styled-background-boat'/>
     </div>
