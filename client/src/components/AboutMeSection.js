bee49a6d8fa5f2d13fffedea94900551624557df
unknown <verwiel.code@gmail.com>
2021-04-23 13:36:21 -0600

    homepage content is aligned at the top on smaller screen sizes

--- client/src/components/AboutMeSection.js
+++ client/src/components/AboutMeSection.js
@@ -9,7 +9,6 @@ const AboutMeSection = () => {
       <section>
         <p className='body-1'>I'm a Wisconsin born, Utah based Developer.</p>
         <p className='body-1'>I am currently working for InsideOut Development as a Application Developer. My days consist mostly of building and helping design our main website and communities</p>
-        <p className='body-1'>Some of my hobbies include: Snowboarding/Skateboarding, Camping, Hiking, and Brewing.</p>
         <Link to='/about'>
           <button className='btn-green'>Want to learn more?</button>
         </Link>
