462b359dcceeed8ba86e1a0e53434a92fa2d0ac2
unknown <verwiel.code@gmail.com>
2021-04-26 14:30:27 -0600

    edited card style

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Project = ({ name, thumbnail, className, openProject }) => {
   return (
-    <>
-      <li className={className} onClick={openProject}>
-        <a href="#projects"><img src={thumbnail} alt={name} />
-          <p>{name}</p>
-        </a>
-      </li>
-    </>
+    <li className={className} onClick={openProject}>
+      <a href="#projects">
+        <img src={thumbnail} alt={name} />
+        <p>{name}</p>
+      </a>
+    </li>
   )
 }
 
