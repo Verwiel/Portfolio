d93ecc47337cbaaf969e85e14159db558ec8ff2b
unknown <verwiel.code@gmail.com>
2021-04-26 13:21:01 -0600

    gave portfolio base about me styles

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import SideBack from '../components/SideBack'
 import ProjectList from '../components/ProjectList'
 import Technologies from '../components/Technologies'
+import ProjectsDrawing from '../assets/projects-drawing.svg'
 
 const Portfolio = () => {
   return (
-    <div className='portfolio'>
-      <h1 id='projects'>Portfolio</h1>
-      <ProjectList />
-      <Technologies /> 
-    </div>
+    <main className='portfolio'>
+      <SideBack route='/#projects' />
+      <header className='portfolio-header'>
+        <article>
+          <h1>Portfolio</h1>
+        </article>
+        <img src={ProjectsDrawing} alt='Portfolio' />
+      </header>
+
+      <article className='portfolio-content'>
+        <section>
+          <ProjectList />
+        </section>
+        <section>
+          <Technologies /> 
+        </section>
+      </article>
+    </main>
   )
 }
 
